5b158a60a54bab85712f8784c75c939a567da82c
An Jaebeom <ajb8533296@gmail.com>
2025-09-12 03:54:03 +0900

    웹소켓 코드 추가

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.api.endpoints import check_fraud, family_group #, websocket
+from app.api.endpoints import check_fraud, family_group, check_fraud_ws
 
 router = APIRouter()
 
 router.include_router(check_fraud.router, prefix="/check_fraud", tags=["check_fraud"])
 router.include_router(family_group.router, prefix="/family_group", tags=["family_group"])
-# router.include_router(websocket.router, prefix="/ws", tags=["websocket"])
+router.include_router(check_fraud_ws.router, prefix="/ws", tags=["websocket"])
